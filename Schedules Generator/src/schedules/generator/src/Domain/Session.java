bbec938accc811dabe8100e6835105f72c8c3916
Sergio Mazzariol <sergio_1582@yahoo.es>
2018-10-15 20:21:45 +0200

    days of the week declaration

--- Schedules Generator/src/schedules/generator/src/Domain/Session.java
+++ Schedules Generator/src/schedules/generator/src/Domain/Session.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -6,9 +6,20 @@
 package schedules.generator.src.Domain;
 
 /**
- *
+ * Session class
  * @author Sergio
  */
 public class Session {
+    
+   //store days of the week
+   private static final int MONDAY = 1;
+   private static final int TUESDAY = 2;
+   private static final int WEDNESDAY = 3;
+   private static final int THURSDAY = 4;
+   private static final int FRIDAY = 5;
+   private static final int SATURDAY = 6;
+   private static final int SUNDAY = 7;
+   
+    
     
 }
