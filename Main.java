c635dbf51b6164263ca4aa69de734aa90cfe27b4
Joaquim Gómez <9joaquimgomez@gmail.com>
2018-11-02 00:33:05 +0100

    Main: Add CtrlPresenter instantation.

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
-import src.*;
+import src.Presenter.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
+        CtrlPresenter ControlPresenter = new CtrlPresenter();
     }
 }
