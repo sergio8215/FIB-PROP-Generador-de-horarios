124503499ebbef82c120541ea9d7b7d0cb272b25
Mireia Cano <canomire@gmail.com>
2018-11-09 16:41:14 +0100

    CtrDomain: basic methods declared

---
+++ source/Domain/CtrlDomain.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package source.Domain;
+
+/**
+ *
+ * @author Sergio
+ */
+public class CtrlDomain {
+
+    private Schedule schedule;
+
+    public CtrlDomain(){
+
+    }
+
+    private void importClassroom(String file){
+
+    }
+
+    public void createScenario(String classroomFile, String subjectFile) {
+
+    }
+
+    /**
+     * list of all the subjects
+     */
+    public void showSubject() {
+
+    }
+
+    /**
+     * list of all the classrooms
+     */
+    public void showClassroom(){
+
+    }
+
+
+   /* creemos que es opcional. queremos mover una asignatura de una hora/aula a otra
+    public void manageUMH() {}
+    */
+
+    public void loadSchedule(String sccheduleFile) {
+
+    }
+
+    public void saveSchedule() {
+
+    }
+
+    public void showSchedule() {
+        //filtrar por quatris, asignaturas, horarios, etc
+    }
+
+    public void generateSchedule(){
+
+    }
+
+    
+}
---
+++ source/Domain/Schedule.java
@@ -0,0 +1,8 @@
+package source.Domain;
+
+public class Schedule {
+
+
+
+    //matriz de asignaturas. Cada fila son los UMH de una asignatura concreta (ordenada por horas/dia)
+}
