4af033690089363bd47e3de028056517b2d4fb47
Mireia Cano <canomire@gmail.com>
2018-11-12 18:13:38 +0100

    ClassroomSession: static call to Session

--- src/Domain/ClassroomSession.java
+++ src/Domain/ClassroomSession.java
@@ -13,13 +13,12 @@ public class ClassroomSession {
     public ClassroomSession(ClassroomSetClass crSet) {
         classroomSessionSet = new ArrayList<UtilsDomain.Pair>();
         ArrayList<ClassroomClass> classroomValues = crSet.getClassroomValues(); //arraylist de les classrooms
-        Session s = new Session(); //instancia de sessio per pillar tota la info general que necessiti
         UtilsDomain.Day arr[] = UtilsDomain.Day.values(); //enum posat en array. aixo no se molt be com gestionar-ho
 
         for(int i = 0; i <classroomValues.size(); ++i) { //per cada classroom...
-            for(int j = 0; j < arr.length; ++j) {   //...cada dia de la setmana...
+            for(int j = 0; j < Session.daysOfTheWeek; ++j) {   //...cada dia de la setmana...
                 //TODO: aquests mètodes no estan a la classr Session
-                for(int k = s.getStartHour(); k < s.getStartHour() + s.getHoursPerDay(); ++k) { //... i cada hora del dia tinc una sessio
+                for(int k = Session.startHour; k < Session.startHour + Session.hoursPerDay; ++k) { //... i cada hora del dia tinc una sessio
                     UtilsDomain.Pair pair = new UtilsDomain.Pair();
                     pair.first = classroomValues.get(i); //el primer valor del pair es una classroom
                     pair.second = new Session(arr[j], k);
