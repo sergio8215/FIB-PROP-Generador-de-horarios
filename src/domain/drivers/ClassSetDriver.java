861d1c0aebbd002908a1007892d97bd00442c62b
Sergio <sergio_1582@yahoo.es>
2018-11-14 19:11:42 +0100

    SetClassDriver

--- src/domain/drivers/ClassSetDriver.java
+++ src/domain/drivers/ClassSetDriver.java
@@ -1,9 +1,6 @@
 package src.domain.drivers;
 
-import src.domain.classes.ClassClass;
-import src.domain.classes.ClassSet;
-import src.domain.classes.Subject;
-import src.domain.classes.SubjectsSet;
+import src.domain.classes.*;
 import src.domain.utils.UtilsDomain;
 import src.domain.utils.inout;
 
@@ -110,12 +107,294 @@ public class ClassSetDriver {
     }
 
 
-    boolean testExistsClass( String subjectName, int group )
-    ClassClass testGetClass(String name, int group )
-    void testAddClass( String key, ClassClass newClass )
-    ArrayList<ClassClass> testUnset()
-    void testClassSort(ArrayList<ClassClass> set)
-    boolean testCompare(ClassClass s1, String op, ClassClass s2)
-    Vector< Vector<String> > testToStr()
+    public static void testExistsClass( ) throws Exception{ i.write(cSet.existsClass( i.readword(), i.readint())); }
+    public static void testGetClass( ) throws Exception{
+        ClassClass myClass = cSet.getClass(i.readword(), i.readint());
+        i.write("ID: "+myClass.getIdentifier()+", ");
+        i.write("Group: "+ myClass.getGroup()+", ");
+        i.write("SubGroup: "+myClass.getSubGroup()+", ");
+        i.write("Quantity Stud: "+myClass.getQuantityStudents()+", ");
+        i.write("Type: "+myClass.getType().toString()+", ");
+        i.write("Shift: "+myClass.getShift().toString()+", ");
+        i.write("Subject name:"+myClass.getSubject().getName());
 
+    }
+    public static void testAddClass( ) throws Exception{
+
+        String identifier;
+        int group;
+        int subGroup;
+        UtilsDomain.ClassType typeG;
+        UtilsDomain.typeShift shift;
+        int quantityStudents;
+
+        String subjectName;
+        int num_students;
+        int level;
+        int[] hoursClasses = new int[3];
+        int[] numberOfGroups = new int[2];
+        UtilsDomain.typeShift shiftSubj;
+
+        i.write("ID: ");
+        identifier = i.readword();
+        i.write("Group: ");
+        group = i.readint();
+        i.write("subGroup: ");
+        subGroup = i.readint();
+        i.write("Type of group 0.Theory, 1.Lab, 2.Problems: ");
+        typeG = UtilsDomain.ClassType.values()[i.readint()];
+        i.write("Shift of group 0.Morning, 1.Afternoon: ");
+        shift = UtilsDomain.typeShift.values()[i.readint()];
+        i.write("Quantity students: ");
+        quantityStudents = i.readint();
+
+        i.write("Subject Name: ");
+        subjectName = i.readword();
+        i.write("Num_students: ");
+        num_students = i.readint();
+        i.write("Level: ");
+        level = i.readint();
+        i.write("Theory_hours: ");
+        hoursClasses[0] = i.readint();
+        i.write("Laboratory_hours: ");
+        hoursClasses[1] = i.readint();
+        i.write("Problems_hours: ");
+        hoursClasses[2] = i.readint();
+        i.write("Number of groups: ");
+        numberOfGroups[0] = i.readint();
+        i.write("Number of Subgroups: ");
+        numberOfGroups[1] = i.readint();
+        i.write("Shift 0.Morning, 1.Afternoon, 2.Both ");
+        shiftSubj = UtilsDomain.typeShift.values()[i.readint()];
+
+        Subject subject = new Subject(subjectName, num_students, level, hoursClasses, numberOfGroups, shiftSubj);
+
+        ClassClass auxClass;
+
+        // Ask for the type of class and then we create the class
+        switch ( typeG.ordinal() ) {
+            case 0:
+                auxClass = new TheoryClass(identifier, subject, group, quantityStudents, shift, subGroup);
+                break;
+            case 1:
+                auxClass = new LaboratoryClass(identifier, subject, group, quantityStudents, shift, subGroup);
+                break;
+            case 2:
+                auxClass = new ProblemsClass(identifier, subject, group, quantityStudents, shift, subGroup);
+                break;
+            default:
+                auxClass = null; // If we cant' find the type of c
+                break;
+        }
+
+        cSet.addClass( identifier, auxClass );
+
+        i.write("Class added to the SetClass. New set size = "+ cSet.size());
+    }
+
+    public static void testUnset() throws Exception{
+        ArrayList<ClassClass> classArray = cSet.unset();
+        for (int ii = 0; ii<classArray.size(); ii++) {
+            // Print the name of each subject
+            i.write(classArray.get(ii).getSubject().getName()+ ", ");
+        }
+    }
+
+    public static void testCompare() throws Exception{
+
+        String identifier;
+        int group;
+        int subGroup;
+        UtilsDomain.ClassType typeG;
+        UtilsDomain.typeShift shift;
+        int quantityStudents;
+
+        String subjectName;
+        int num_students;
+        int level;
+        int[] hoursClasses = new int[3];
+        int[] numberOfGroups = new int[2];
+        UtilsDomain.typeShift shiftSubj;
+
+        i.write("ID: ");
+        identifier = i.readword();
+        i.write("Group: ");
+        group = i.readint();
+        i.write("subGroup: ");
+        subGroup = i.readint();
+        i.write("Type of group 0.Theory, 1.Lab, 2.Problems: ");
+        typeG = UtilsDomain.ClassType.values()[i.readint()];
+        i.write("Shift of group 0.Morning, 1.Afternoon: ");
+        shift = UtilsDomain.typeShift.values()[i.readint()];
+        i.write("Quantity students: ");
+        quantityStudents = i.readint();
+
+        i.write("Subject Name: ");
+        subjectName = i.readword();
+        i.write("Num_students: ");
+        num_students = i.readint();
+        i.write("Level: ");
+        level = i.readint();
+        i.write("Theory_hours: ");
+        hoursClasses[0] = i.readint();
+        i.write("Laboratory_hours: ");
+        hoursClasses[1] = i.readint();
+        i.write("Problems_hours: ");
+        hoursClasses[2] = i.readint();
+        i.write("Number of groups: ");
+        numberOfGroups[0] = i.readint();
+        i.write("Number of Subgroups: ");
+        numberOfGroups[1] = i.readint();
+        i.write("Shift 0.Morning, 1.Afternoon, 2.Both ");
+        shiftSubj = UtilsDomain.typeShift.values()[i.readint()];
+
+        Subject subject = new Subject(subjectName, num_students, level, hoursClasses, numberOfGroups, shiftSubj);
+
+        ClassClass s1;
+
+        // Ask for the type of class and then we create the class
+        switch ( typeG.ordinal() ) {
+            case 0:
+                s1 = new TheoryClass(identifier, subject, group, quantityStudents, shift, subGroup);
+                break;
+            case 1:
+                s1 = new LaboratoryClass(identifier, subject, group, quantityStudents, shift, subGroup);
+                break;
+            case 2:
+                s1 = new ProblemsClass(identifier, subject, group, quantityStudents, shift, subGroup);
+                break;
+            default:
+                s1 = null; // If we cant' find the type of c
+                break;
+        }
+
+        i.write("Operator: ");
+        String op = i.readword();
+
+        i.write("ID: ");
+        identifier = i.readword();
+        i.write("Group: ");
+        group = i.readint();
+        i.write("subGroup: ");
+        subGroup = i.readint();
+        i.write("Type of group 0.Theory, 1.Lab, 2.Problems: ");
+        typeG = UtilsDomain.ClassType.values()[i.readint()];
+        i.write("Shift of group 0.Morning, 1.Afternoon: ");
+        shift = UtilsDomain.typeShift.values()[i.readint()];
+        i.write("Quantity students: ");
+        quantityStudents = i.readint();
+
+        i.write("Subject Name: ");
+        subjectName = i.readword();
+        i.write("Num_students: ");
+        num_students = i.readint();
+        i.write("Level: ");
+        level = i.readint();
+        i.write("Theory_hours: ");
+        hoursClasses[0] = i.readint();
+        i.write("Laboratory_hours: ");
+        hoursClasses[1] = i.readint();
+        i.write("Problems_hours: ");
+        hoursClasses[2] = i.readint();
+        i.write("Number of groups: ");
+        numberOfGroups[0] = i.readint();
+        i.write("Number of Subgroups: ");
+        numberOfGroups[1] = i.readint();
+        i.write("Shift 0.Morning, 1.Afternoon, 2.Both ");
+        shiftSubj = UtilsDomain.typeShift.values()[i.readint()];
+
+        Subject subject2 = new Subject(subjectName, num_students, level, hoursClasses, numberOfGroups, shiftSubj);
+
+        ClassClass s2;
+
+        // Ask for the type of class and then we create the class
+        switch ( typeG.ordinal() ) {
+            case 0:
+                s2 = new TheoryClass(identifier, subject2, group, quantityStudents, shift, subGroup);
+                break;
+            case 1:
+                s2 = new LaboratoryClass(identifier, subject2, group, quantityStudents, shift, subGroup);
+                break;
+            case 2:
+                s2 = new ProblemsClass(identifier, subject2, group, quantityStudents, shift, subGroup);
+                break;
+            default:
+                s2 = null; // If we cant' find the type of c
+                break;
+        }
+
+
+        i.write( ClassSet.compare(s1, op, s2));
+
+    }
+
+    public static void testToStr() throws Exception{
+
+        Vector< Vector<String> > myStringVector = cSet.toStr();
+        for( Vector<String> c: myStringVector ){
+            for (String r: c){
+                i.write(r+", ");
+            }
+            i.writeln("\n");
+        }
+    }
+
+    public static void main (String [] args) throws Exception {
+
+        int option = 0;
+
+        while( option != 9) {
+
+            i.write("------------------------------------------ \n");
+            i.write("------------------MENU-------------------- \n");
+            i.write("------------------------------------------ \n");
+            i.write("1 ->Test ConstructorSubjects: \n");
+            i.write("2 ->Test ConstructorFromStr: \n");
+            i.write("3 ->Test ExistsClass: \n");
+            i.write("4 ->Test GetClass: \n");
+            i.write("5 ->Test AddClass: \n");
+            i.write("6 ->Test Unset: \n");
+            i.write("7 ->Test Compare: \n");
+            i.write("8 ->Test ToStr: \n");
+            i.write("9 ->Exit:  \n");
+
+            option = i.readint();
+
+
+            switch (option) {
+                case 1:
+                    testConstructorSubjects();
+                    System.in.read();
+                    break;
+                case 2:
+                    testConstructorFromStr();
+                    System.in.read();
+                    break;
+                case 3:
+                    testExistsClass();
+                    System.in.read();
+                    break;
+                case 4:
+                    testGetClass();
+                    System.in.read();
+                    break;
+                case 5:
+                    testAddClass();
+                    System.in.read();
+                    break;
+                case 6:
+                    testUnset();
+                    System.in.read();
+                    break;
+                case 7:
+                    testCompare();
+                    System.in.read();
+                    break;
+                case 8:
+                    testToStr();
+                    System.in.read();
+                    break;
+            }
+        }
+    }
 }
