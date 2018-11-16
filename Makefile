6dc5dbf7b43e59509cae6414f6d2fcb943ba7617
Joaquim Gómez <9joaquimgomez@gmail.com>
2018-11-16 11:33:51 +0100

    Over and over again!

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 make:
 	javac -d bin/ -cp libs/json-simple-1.1.1.jar src/domain/utils/*.java src/domain/classes/*.java src/domain/comparators/*.java src/domain/controllers/*.java src/persistence/*.java src/presentation/*.java
 	javac -d bin/ src/domain/drivers/*.java
-	javac -d bin/ -cp libs/junit-4.12.jar:libs/hamcrest-core-1.3.jar:bin/ src/domain/junits/*.java
+	javac -d bin/ -cp libs/*.jar:bin/  src/domain/junits/*.java
 
 run:
 	java -cp bin/:libs/json-simple-1.1.1.jar src.presentation.CtrlPresenter
--- src/domain/classes/Constraints.java
+++ src/domain/classes/Constraints.java
@@ -1,20 +1,41 @@
 package src.domain.classes;
 
-import src.domain.utils.UtilsDomain;
 import src.domain.utils.UtilsDomain.*;
 
+/**
+ * Constraints Class.
+ * @author Joaquim Gómez & Mireia Cano
+ */
 public class Constraints {
 
     // UNARY CONSTRAINTS
 
+    /**
+     * Unary Constraint: Size Classroom Constraint.
+     * @param m MUS to try the constraint with the pair Classroom-Session.
+     * @param cs Pair Classroo-Session to try the contraint with the MUS.
+     * @return True if satisfied constraint.
+     */
     public static boolean sizeClassroomUnaryConstraint(MUS m, Pair<Classroom, Session> cs) {
         return cs.first.getCapacity() >= m.getClassClass().getQuantityStudents();
     }
 
+    /**
+     * Unary Constraint: Type Classroom Constraint.
+     * @param m MUS to try the constraint with the pair Classroom-Session.
+     * @param cs Pair Classroo-Session to try the contraint with the MUS.
+     * @return True if satisfied constraint.
+     */
     public static boolean typeClassroomUnaryConstraint(MUS m, Pair<Classroom, Session> cs) {
         return cs.first.getType() == m.getClassClass().getType();
     }
 
+    /**
+     * Unary Constraint: Shift Class Constraint
+     * @param m MUS to try the constraint with the pair Classroom-Session.
+     * @param cs Pair Classroo-Session to try the contraint with the MUS.
+     * @return True if satisfied constraint.
+     */
     public static boolean shiftClassUnaryConstraint(MUS m, Pair<Classroom, Session> cs) {
         if (m.getClassClass().getShift() == typeShift.MORNING ) return cs.second.getHour() < 14;
         else if (m.getClassClass().getShift() == typeShift.AFTERNOON ) return cs.second.getHour() >= 14;
@@ -26,6 +47,12 @@ public class Constraints {
 
     // N-ARY CONSTRAINTS
 
+    /**
+     * N-ary Constraint: Not Same Classroom snd Session.
+     * @param m1 First MUS to try the constraint.
+     * @param m2 Second MUS to try the constraint.
+     * @return True if satisfied constraint.
+     */
     public static boolean notSameClassroomAndSession(MUS m1, MUS m2) {
         if((Session.compare(m1.getSession(), "==", m2.getSession())) &&
            (m1.getClassroom().getName().equals(m2.getClassroom().getName())))
@@ -33,6 +60,12 @@ public class Constraints {
         return true;
     }
 
+    /**
+     * N-ary Constraint: Theory and Lab of Class no Together.
+     * @param m1 First MUS to try the constraint.
+     * @param m2 Second MUS to try the constraint.
+     * @return True if satisfied constraint.
+     */
     public static boolean theoryAndLabsOfClassNoTogether(MUS m1, MUS m2) {
         // Si m1 de un tipo(t/p/l) igual que el tipo de m2 (t/p/l) y en la misma sesion => false
         if (m1.getClassClass().getType() == m2.getClassClass().getType() &&
@@ -43,6 +76,12 @@ public class Constraints {
         return true;
     }
 
+    /**
+     * N-ary Constraint: Theorys of Subjects Of Same Level no Together.
+     * @param m1 First MUS to try the constraint.
+     * @param m2 Second MUS to try the constraint.
+     * @return True if satisfied constraint.
+     */
     public static boolean theorysOfSubjectsOfSameLevelNoTogether(MUS m1, MUS m2) {
         if (m1.getClassClass().getType() == ClassType.THEORY &&
                 m2.getClassClass().getType() == ClassType.THEORY &&
@@ -53,6 +92,12 @@ public class Constraints {
         return true;
     }
 
+    /**
+     * N-ary Constraint: Theory of Subject From Different Classes no Together
+     * @param m1 First MUS to try the constraint.
+     * @param m2 Second MUS to try the constraint.
+     * @return True if satisfied constraint.
+     */
     public static boolean theoryOfSubjectFromDifferentClassesNoTogether(MUS m1, MUS m2) {
         if (m1.getClassClass().getType() == ClassType.THEORY &&
                 m2.getClassClass().getType() == ClassType.THEORY &&
@@ -62,6 +107,12 @@ public class Constraints {
         return true;
     }
 
+    /**
+     * N-ary Constraint: Labs and Problems From Different Subjects of Same Group no Together
+     * @param m1 First MUS to try the constraint.
+     * @param m2 Second MUS to try the constraint.
+     * @return True if satisfied constraint.
+     */
     public static boolean LabsAndProblemsFromDifferentSubjectsOfSameGroupNoTogether(MUS m1, MUS m2) {
         if ((m1.getClassClass().getType() == ClassType.LABORATORY ||
                 m1.getClassClass().getType() == ClassType.PROBLEMS) &&
